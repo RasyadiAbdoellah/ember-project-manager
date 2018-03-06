d3a3aedfa2fbcf61c07c2f24414fed9c93432b11
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-03-06 17:45:43 -0500

    Create computed properties for dates and project length

--- app/models/project.js
+++ app/models/project.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
@@ -6,5 +7,25 @@ export default DS.Model.extend({
   dueDate: DS.attr('date'),
   startDate: DS.attr('date'),
   projectLength:DS.attr('number'),
-  user: DS.belongsTo('user')
+  user: DS.belongsTo('user'),
+
+  formattedDueDate: computed('dueDate', function () {
+    return this.get('dueDate').toLocaleDateString()
+  }),
+  formattedDueTime: computed('dueDate', function () {
+    return this.get('dueDate').toLocaleTimeString()
+  }),
+
+  formattedStartDate: computed('startDate', function () {
+    return this.get('startDate').toLocaleDateString()
+  }),
+  formattedStartTime: computed('startDate', function () {
+    return this.get('startDate').toLocaleTimeString()
+  }),
+  formattedProjectLength: computed('projectLength', function () {
+    // returns formatted project length
+    return `${this.get('projectLength')/(3600 * 24)} days`
+  })
+
+
 });
